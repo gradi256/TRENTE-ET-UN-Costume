7d9c046d8472ef8afddd89379639c44b99a7fbdc
Gradi <gradinawej8@gmail.com>
2025-12-26 18:55:35 +0100

    modification du header et code du hero

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/**Fonctionnalité pour le bouton menu */
+let boutonMenu = document.querySelector(".boutonMenu");
+let navbar = document.querySelector(".navbar");
+
+boutonMenu.addEventListener("click", () => {
+    navbar.classList.add = "navbar2";
+});
